c8d37e09155a51ec065a612c26c14f5002f6a886
Hiroki Ito <hiroki110@gmail.com>
2017-03-06 01:49:53 +0900

    First commit

---
+++ Readme.md
@@ -0,0 +1,30 @@
+# Shu-ho standalone
+
+Shu-ho standalone is a simple weekly report editor.
+
+## Feature
+
+- Plain text based editor
+- Customizable templates
+- Sending a report by mail
+
+## How to start 
+
+1. download a latest release from "release" tab
+2. open the download zip and execute Shu-ho executable
+   or 'npm start' in the shell
+
+## Changelog
+
+### [0.0.1] - 2017-03-03
+
+- first commit
+
+
+## Future work
+
+
+## LICENSE
+
+This software is released under the MIT License, see LICENSE.
+
---
+++ main.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+// for auto reloading in dev env
+require('electron-reload')(__dirname);
+
+const path = require('path');
+const url = require('url');
+ 
+// Main window 
+let mainWindow;
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 1024, height: 800})
+ 
+  // use index.html as main page
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+ 
+  // invoke dev tools 
+  mainWindow.webContents.openDevTools();
+ 
+  // window is closed
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+
+//  app is ready
+app.on('ready', createWindow);
+ 
+// all windows are closed
+app.on('window-all-closed', function () {
+  // macOSのとき以外はアプリケーションを終了させます
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+// application is activated. [Mac] The dock icon is clicked.
+app.on('activate', function () {
+  /// when we dont have main window, create it.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
