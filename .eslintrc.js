e152951cd75352e3ec39edc660d2f2d3dc818585
Hiroki Ito <hiroki110@gmail.com>
2017-03-16 06:18:01 +0900

    Changed default style to vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-    "extends": "standard",
+    "extends": ["vue"],
     "plugins": [
-        "standard",
         "promise"
-    ]
+    ],
+	"rules": {
+		"semi": ["warn", "always"]
+	}
 };
