c2b25beb9d430ee1eca38422b1c5b159b369d7d6
Hiroki Ito <hiroki110@gmail.com>
2017-03-20 08:37:44 +0900

    Fixed require->import, chhanged variable name

--- test/save-and-load.js
+++ test/save-and-load.js
@@ -1,27 +1,26 @@
 'use strict'
 
-const chai = require('chai')
+import chai from 'chai'
 const should = chai.should
 const expect = chai.expect
-const assert = chai.assert
 
-const target = require('../js/service/file-io')
+import fileio from '../js/service/file-io'
 
 describe('list files', function () {
-  const list = target.getFilenames('.')
+  const list = fileio.getFilenames('.')
   console.log(list)
 
   it('reads directory without error', function () {
-    expect(target.getFilenames('.')).to.be.a('array')
+    expect(fileio.getFilenames('.')).to.be.a('array')
   })
 
   it('returns at least 3 filenames', function () {
-    expect(target.getFilenames('.')).to.have.length.of.at.least(3)
+    expect(fileio.getFilenames('.')).to.have.length.of.at.least(3)
   })
 
   it('throws an error when directory does not exist', function () {
     try {
-      target.getFilenames('/nowhere')
+      fileio.getFilenames('/nowhere')
     } catch (err) {
       expect(err).to.have.property('code').equal('ENOENT')
     }
