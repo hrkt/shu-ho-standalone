b69570a623e2cd9b793a966e5a2daa30813bad6a
Hiroki Ito <hiroki110@gmail.com>
2017-03-19 09:59:15 +0900

    Add plugin for tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,11 @@
 module.exports = {
-    "extends": ["vue"],
-    "plugins": [
-        "promise"
-    ],
-	"rules": {
-		"semi": ["warn", "always"]
-	}
-};
+  "extends": ["vue"],
+  "plugins": [
+    "promise"
+    "mocha"
+    "chai-expect"
+  ],
+  "rules": {
+    "mocha/no-exclusive-tests": "error"
+  }
+}
