cc27e15ec255147a6228eb9a60b22058e0119e12
Hiroki Ito <hiroki110@gmail.com>
2017-04-08 17:32:22 +0900

    #143209677 lint

--- src/js/template-generator.js
+++ src/js/template-generator.js
@@ -5,7 +5,7 @@ import * as dateUtil from './date-util.js'
 
 moment.locale('ja', {
   weekdays: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
-  weekdaysShort: ['日', '月', '火', '水', '木', '金', '土'],
+  weekdaysShort: ['日', '月', '火', '水', '木', '金', '土']
 })
 
 function week (startDate) {
