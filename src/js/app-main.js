8e3fca3515b03eca69a44c7a31aeffd6d96c1a84
Hiroki Ito <hiroki110@gmail.com>
2017-04-22 09:25:11 +0900

    [#143991325] Add router page

--- src/js/app-main.js
+++ src/js/app-main.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
 import App from '../App.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const Test1 = { template: '<div>test1</div>' }
+const Test2 = { template: '<div>test2</div>' }
+
+const routes = [
+  { path: '/', component: App },
+  { path: '/test1', component: Test1 },
+  { path: '/test2', component: Test2 }
+]
+
+const router = new VueRouter({
+  routes: routes
+})
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router
 })
